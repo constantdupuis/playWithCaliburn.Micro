5b3d10f883a02b9c86bfd8943fecf994f958c45a
Constant Dupuis <31339604+constantdupuis@users.noreply.github.com>
2020-04-03 19:11:12 +0200

    Finish training

--- WithDotNETFramework/ViewModels/ShellViewModel.cs
+++ WithDotNETFramework/ViewModels/ShellViewModel.cs
@@ -4,12 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WithDotNETFramework.Models;
 
 namespace WithDotNETFramework.ViewModels
 {
-    public class ShellViewModel : Screen
+    public class ShellViewModel : Conductor<object>
     {
         private string _firstName = "Constant";
+        private string _lastName = "Dupuis";
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+        private PersonModel _selectedPerson;
+
+        public ShellViewModel()
+        {
+            People.Add(new PersonModel { FirstName="Marcel", LastName="Dufour"});
+            People.Add(new PersonModel { FirstName="Raoul", LastName="Delforge"});
+            People.Add(new PersonModel { FirstName="Thérése", LastName="Druot"});
+        }
 
         public string FirstName
         {
@@ -21,8 +32,6 @@ namespace WithDotNETFramework.ViewModels
                 NotifyOfPropertyChange(() => FullName);
             }
         }
-
-        private string _lastName = "Dupuis";
 
         public string LastName
         {
@@ -39,5 +48,39 @@ namespace WithDotNETFramework.ViewModels
             get { return $"{FirstName} {LastName}"; }
         }
 
+        public BindableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+        public PersonModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set { _selectedPerson = value;
+                NotifyOfPropertyChange(() => SelectedPerson); ;
+            }
+        }
+
+        public bool CanClearText(string firstName, string lastName)
+        {
+            return !string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName);
+        }
+        
+        public void ClearText(string firstName, string lastName)
+        {
+            FirstName = "";
+            LastName = "";
+        }
+
+        public void LoadPageOne()
+        {
+            ActivateItem( new FirstChildViewModel());
+        }
+
+        public void LoadPageTwo()
+        {
+            ActivateItem(new SecondChildViewModel());
+        }
     }
 }
